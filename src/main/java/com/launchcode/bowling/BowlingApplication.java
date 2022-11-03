f7ed2de1f0929c4b517991c036900412edc33948
cashhahuynh <cashhahuynh@gmail.com>
2022-11-03 19:23:56 -0400

    Initial commit

---
+++ src/main/java/com/launchcode/bowling/BowlingApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.bowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchcode/bowling/BowlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.bowling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BowlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
