4715a23028159d03b9dfaafb079f4a036d9217c6
Thomas McKune <tommckune@yahoo.com>
2022-11-11 18:38:42 -0500

    added  new test controller

--- src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
+++ src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
@@ -2,16 +2,23 @@ package com.launchcode.bowling.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
+=======
+
+@Controller
+public class HelloWorldController {
+>>>>>>> added MySQL connections
     @GetMapping(value = "")
     public String welcome() {
         return "hello";
     }
+<<<<<<< HEAD
 
     @GetMapping("test")
     @ResponseBody
@@ -19,4 +26,6 @@ public class HelloWorldController {
         return "something";
     }
 
+=======
+>>>>>>> added MySQL connections
 }
