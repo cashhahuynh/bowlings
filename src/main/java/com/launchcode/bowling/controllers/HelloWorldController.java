f3f8bb020655b76a1e4a664f647645790a392f92
cashhahuynh <cashhahuynh@gmail.com>
2022-11-11 18:47:52 -0500

    11/11/22 commit

--- src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
+++ src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
@@ -2,30 +2,11 @@ package com.launchcode.bowling.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
-
-=======
-
-@Controller
-public class HelloWorldController {
->>>>>>> added MySQL connections
     @GetMapping(value = "")
     public String welcome() {
         return "hello";
     }
-<<<<<<< HEAD
-
-    @GetMapping("test")
-    @ResponseBody
-    public String test() {
-        return "something";
-    }
-
-=======
->>>>>>> added MySQL connections
 }
