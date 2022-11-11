428143e3da2541f5f0315d6ec81431f3efcdde6b
Thomas McKune <tommckune@yahoo.com>
2022-11-11 18:32:13 -0500

    added test controller

---
+++ src/main/java/com/launchcode/bowling/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.launchcode.bowling.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    @GetMapping(value = "test")
+    @ResponseBody
+    public String test() {
+        return "test worked";
+    }
+}
