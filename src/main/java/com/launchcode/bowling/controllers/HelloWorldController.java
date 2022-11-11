43fc3d620c871783e3d897e1a5766040c256ed2d
Thomas McKune <tommckune@yahoo.com>
2022-11-11 18:28:19 -0500

    added MySQL connections

--- src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
+++ src/main/java/com/launchcode/bowling/controllers/HelloWorldController.java
@@ -2,15 +2,24 @@ package com.launchcode.bowling.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorldController {
 
+=======
+
+@Controller
+public class HelloWorldController {
+>>>>>>> added MySQL connections
     @GetMapping(value = "")
     public String welcome() {
         return "hello";
     }
+<<<<<<< HEAD
 
+=======
+>>>>>>> added MySQL connections
 }
