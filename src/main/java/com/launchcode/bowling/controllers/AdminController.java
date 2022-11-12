a2fb8cd5ef12d55da9eb1895a24bcd2f555839b2
cashhahuynh <cashhahuynh@gmail.com>
2022-11-12 12:35:35 -0500

    log in and registration commit

--- src/main/java/com/launchcode/bowling/controllers/AdminController.java
+++ src/main/java/com/launchcode/bowling/controllers/AdminController.java
@@ -3,14 +3,14 @@ package com.launchcode.bowling.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloWorldController {
+@RequestMapping("register")
+public class AdminController {
 
-    @GetMapping(value = "")
-    public String welcome() {
-        return "hello";
-    }
+    @GetMapping(value = "admin")
+        public String register() {
+            return "admin/register";
+        }
 
 }
---
+++ src/main/java/com/launchcode/bowling/models/data/AdminRepository.java
@@ -0,0 +1,11 @@
+package com.launchcode.bowling.models.data;
+
+import com.launchcode.bowling.models.Admin;
+import org.springframework.stereotype.Repository;
+
+//@Repository
+//public interface AdminRepository extends CrudRepository<Admin, Integer> {
+//
+//    //something
+//
+//}
