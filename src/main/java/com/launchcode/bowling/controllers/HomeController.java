136b7a92a781f8dbb9ea948593ce39549603d2dc
cashhahuynh <cashhahuynh@gmail.com>
2022-11-12 12:35:35 -0500

    log in and registration commit

---
+++ src/main/java/com/launchcode/bowling/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.launchcode.bowling.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "")
+    public String homepage() {
+        return "index";
+    }
+
+    @GetMapping(value = "register")
+    public String register() {
+        return "registrationType";
+    }
+
+}
