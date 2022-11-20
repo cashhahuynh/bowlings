cc44eb387f0c4d6b1c78361c0102769e2952dfd0
Thomas McKune <tommckune@yahoo.com>
2022-11-20 18:32:22 -0500

    added DTO's and some html

--- src/main/java/com/launchcode/bowling/models/AbstractEntity.java
+++ src/main/java/com/launchcode/bowling/models/AbstractEntity.java
@@ -1,4 +1,34 @@
 package com.launchcode.bowling.models;
 
-public class AbstractEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) obj;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
