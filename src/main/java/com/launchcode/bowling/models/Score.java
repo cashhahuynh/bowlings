8440260014224614aa04734d410dc104f19ac73d
Thomas McKune <tommckune@yahoo.com>
2022-11-26 14:24:29 -0500

    Trying to set it up just to run with user and score model

--- src/main/java/com/launchcode/bowling/models/Score.java
+++ src/main/java/com/launchcode/bowling/models/Score.java
@@ -1,4 +1,30 @@
 package com.launchcode.bowling.models;
 
-public class Score {
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Score extends AbstractEntity {
+
+    @Size(max=300)
+    private Integer score;
+
+    public Score(){}
+
+    public Score(Integer score) {
+        this.score = score;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return score.toString();
+    }
 }
---
+++ src/main/java/com/launchcode/bowling/models/data/ScoreRepository.java
@@ -0,0 +1,13 @@
+package com.launchcode.bowling.models.data;
+
+import com.launchcode.bowling.models.Score;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface ScoreRepository extends CrudRepository<Score, Integer> {
+}
+
